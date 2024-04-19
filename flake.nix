235f84890457221145214ba055246a1a57c0a98f
wldhx <wldhx+github.com@wldhx.me>
2024-04-19 21:51:51 +0400

    nix: Remove poetry2nix

--- flake.nix
+++ flake.nix
@@ -4,26 +4,15 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
       in
       {
-        packages = {
-          myapp = mkPoetryApplication { projectDir = ./.; };
-          default = self.packages.${system}.myapp;
-        };
-
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.myapp ];
           packages = with pkgs; [ poetry dhall-json ];
         };
       });
